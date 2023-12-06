91050335a04eef90f03ba49b1c39244ba15293d1
kajal-shar <147511175+kajal-shar@users.noreply.github.com>
2023-12-06 22:12:21 +0530

    Add files via upload

---
+++ reversenatural.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class reversenatural{
+    public static void main(String[] args) {
+        int i,n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=n;i>=1;i--){
+            System.out.println(i);
+
+        }
+
+        
+    }
+}
